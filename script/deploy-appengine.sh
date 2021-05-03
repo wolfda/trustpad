784d63f2555a7e92a86af3d03d4ce0078abc36d8
Daniel Wolf <daniel@daniels-mbp.lan>
2021-05-03 15:29:06 -0700

    Adds a privacy policy

--- script/deploy-appengine.sh
+++ script/deploy-appengine.sh
@@ -2,4 +2,5 @@
 # Deploy the master branch live on appengine.
 # Before running this script, python appengine SDK needs to be installed first. See https://cloud.google.com/appengine/downloads
 
-appcfg.py update appengine-root/
+gcloud config set project trust-pad
+gcloud app deploy appengine-root/app.yaml
