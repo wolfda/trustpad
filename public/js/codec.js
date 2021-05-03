13543684321ee6caf25db4074175952cba9e8d0b
Daniel Wolf <daniel@daniels-mbp.lan>
2021-05-03 15:53:16 -0700

    Migrate javascript to ES6

--- public/js/codec.js
+++ public/js/codec.js
@@ -3,59 +3,67 @@
  * @author Daniel Wolf
  */
 
-/**
- * @param {string} passphrase The secret passphrase
- */
-function Codec(passphrase) {
-  this.passphrase = passphrase;
+const KEY_SIZE = 256;
+const PBKD2_ITER = 1000;
+const TAG = 'trustpad';
+const TAG_SIZE = 64;
+
+class Codec {
+    passphrase;
+
+    /**
+     * @param {string} passphrase The secret passphrase
+     */
+    constructor(passphrase) {
+      this.passphrase = passphrase;
+    }
+
+    /**
+     * @param {string} plaintext The string to encrypt
+     */
+    encrypt(plaintext) {
+        let iv = sjcl.random.randomWords(4, 0);
+        let salt = sjcl.random.randomWords(2, 0);
+        let key = sjcl.misc.pbkdf2(this.passphrase, salt, PBKD2_ITER, KEY_SIZE, sjcl.misc.hmac);
+        let prp = new sjcl.cipher.aes(key);
+        let data = sjcl.codec.utf8String.toBits(plaintext);
+        let encrypted = sjcl.mode.ccm.encrypt(prp, data, iv, TAG, TAG_SIZE);
+
+        let params = {
+          'encrypted': sjcl.codec.base64.fromBits(encrypted),
+          'iv': sjcl.codec.base64.fromBits(iv),
+          'salt': sjcl.codec.base64.fromBits(salt),
+          'pbkdf2Iter': Codec.PBKD2_ITER,
+          'keySize': KEY_SIZE,
+          'tagSize': TAG_SIZE
+        };
+        return JSON.stringify(params);
+    }
+
+    /**
+     * @param {string} data The encoded data previously returned by encrypt()
+     */
+    decrypt(data) {
+        let params = JSON.parse(data);
+        let iv = sjcl.codec.base64.toBits(params.iv);
+        let salt = sjcl.codec.base64.toBits(params.salt);
+        let iter = params.pbkdf2Iter;
+        let keySize = params.keySize;
+        let tagSize = params.tagSize;
+        let key = sjcl.misc.pbkdf2(this.passphrase, salt, iter, keySize, sjcl.misc.hmac);
+        let prp = new sjcl.cipher.aes(key);
+        let ciphertext = sjcl.codec.base64.toBits(params.encrypted);
+
+        try {
+            let decrypted = sjcl.mode.ccm.decrypt(prp, ciphertext, iv, TAG, tagSize);
+            return sjcl.codec.utf8String.fromBits(decrypted);
+        } catch (err) {
+            console.log('Decryption failed');
+            return null;
+        }
+    }
 }
 
-Codec.KEY_SIZE = 256;
-Codec.PBKD2_ITER = 1000;
-Codec.TAG = 'trustpad';
-Codec.TAG_SIZE = 64;
 
-/**
- * @param {string} plaintext The string to encrypt
- */
-Codec.prototype.encrypt = function(plaintext) {
-    var iv = sjcl.random.randomWords(4, 0);
-    var salt = sjcl.random.randomWords(2, 0);
-    var key = sjcl.misc.pbkdf2(this.passphrase, salt, Codec.PBKD2_ITER, Codec.KEY_SIZE, sjcl.misc.hmac);
-    var prp = new sjcl.cipher.aes(key);
-    var data = sjcl.codec.utf8String.toBits(plaintext);
-    var encrypted = sjcl.mode.ccm.encrypt(prp, data, iv, Codec.TAG, Codec.TAG_SIZE);
 
-    var params = {
-      'encrypted': sjcl.codec.base64.fromBits(encrypted),
-      'iv': sjcl.codec.base64.fromBits(iv),
-      'salt': sjcl.codec.base64.fromBits(salt),
-      'pbkdf2Iter': Codec.PBKD2_ITER,
-      'keySize': Codec.KEY_SIZE,
-      'tagSize': Codec.TAG_SIZE
-    };
-    return JSON.stringify(params);
-};
 
-/**
- * @param {string} data The encoded data previously returned by encrypt()
- */
-Codec.prototype.decrypt = function(data) {
-    var params = JSON.parse(data);
-    var iv = sjcl.codec.base64.toBits(params.iv);
-    var salt = sjcl.codec.base64.toBits(params.salt);
-    var iter = params.pbkdf2Iter;
-    var keySize = params.keySize;
-    var tagSize = params.tagSize;
-    var key = sjcl.misc.pbkdf2(this.passphrase, salt, iter, keySize, sjcl.misc.hmac);
-    var prp = new sjcl.cipher.aes(key);
-    var ciphertext = sjcl.codec.base64.toBits(params.encrypted);
-
-    try {
-        var decrypted = sjcl.mode.ccm.decrypt(prp, ciphertext, iv, Codec.TAG, tagSize);
-        return sjcl.codec.utf8String.fromBits(decrypted);
-    } catch (err) {
-        console.log('Decryption failed');
-        return null;
-    }
-};
--- public/js/gdrive.js
+++ public/js/gdrive.js
@@ -3,120 +3,122 @@
  * @author Daniel Wolf
  */
 
-function GDrive() {
-    this.ready = false;
-    this.delayedQueue = [];
+const API_KEY = 'AIzaSyCIfNCqRxgnd5xanK9oCtv8olMOF9187yQ';
+const CLIENT_ID = '170791641914-aihfsicei215ult5r0qjmq5gg0jqn4k6.apps.googleusercontent.com';
+const SCOPES = 'https://www.googleapis.com/auth/drive';
 
-    gapi.client.setApiKey(GDrive.API_KEY);
-    gapi.client.load('drive', 'v2', this.setReady.bind(this));
+class GDrive {
+    ready = false;
+    delayedQueue = [];
+
+    constructor() {
+        gapi.client.setApiKey(API_KEY);
+        gapi.client.load('drive', 'v2', this.setReady.bind(this));
+    }
+
+    /**
+     * Delay the execution of an api method after the api is ready.
+     */
+    whenReadyCall(delayedFunction) {
+        this.delayedQueue.push(delayedFunction);
+    }
+
+    /**
+     * Notify that the drive api is ready to be called. Processes any pending calls than may have been queued up.
+     */
+    setReady() {
+        this.ready = true;
+        this.delayedQueue.forEach(item => item());
+        this.delayedQueue = null;
+    }
+
+    /**
+     * Check if the current user has authorized the application.
+     */
+    auth(immediate, callback) {
+        gapi.auth.authorize({
+            'client_id': CLIENT_ID,
+            'scope': SCOPES,
+            'immediate': immediate
+        }, function(authResult) {
+            callback(authResult && !authResult.error);
+        });
+    }
+
+    listTrustpadFiles(callback) {
+        if (!this.ready) {
+            this.whenReadyCall(this.listTrustpadFiles.bind(this, callback));
+            return;
+        }
+
+        let request = gapi.client.drive.files.list({
+            'q': 'title contains \'.trustpad\' and not trashed'
+        }).then(
+            function(res) {
+              console.log(res.result);
+              callback(res.result.items);
+            },
+            function(error) {
+              console.log('error: ' + error);
+            }
+        );
+    }
+
+    writeFile(fileId, data, callback) {
+        const boundary = '-------314159265358979323846';
+        const delimiter = "\r\n--" + boundary + "\r\n";
+        const closeDelim = "\r\n--" + boundary + "--";
+        let metadata = {
+            'title': 'test.trustpad',
+            'mimeType': 'text/plain'
+        };
+        let body = delimiter +
+            'Content-Type: application/json\r\n\r\n' +
+            JSON.stringify(metadata) +
+            delimiter +
+            'Content-Type: text/plain\r\n' +
+            'Content-Transfer-Encoding: base64\r\n' +
+            '\r\n' +
+            btoa(data) +
+            closeDelim;
+        let url = '/upload/drive/v2/files';
+        if (fileId) {
+            url += '/' + fileId;
+        }
+        let request = gapi.client.request({
+            'path': url,
+            'method': fileId ? 'PUT' : 'POST',
+            'params': { 'uploadType': 'multipart' },
+            'headers': { 'Content-Type': 'multipart/mixed; boundary="' + boundary + '"' },
+            'body': body
+        }).then(
+            function(res) {
+                callback(res.id);
+            },
+            function(error) {
+                console.log('error: ' + error);
+            }
+        );
+    }
+
+    readFile(fileId, callback) {
+        if (!this.ready) {
+            this.whenReadyCall(this.readFile.bind(this, fileId, callback));
+            return;
+        }
+
+        let request = gapi.client.drive.files.get({
+            'fileId': fileId,
+            'alt': 'media'
+        }).then(
+            function(res) {
+                callback(res.body);
+            },
+            function(error) {
+                console.log('error: ' + error);
+            }
+        );
+    }
 }
 
-GDrive.API_KEY = 'AIzaSyCIfNCqRxgnd5xanK9oCtv8olMOF9187yQ';
-GDrive.CLIENT_ID = '170791641914-aihfsicei215ult5r0qjmq5gg0jqn4k6.apps.googleusercontent.com';
-GDrive.SCOPES = 'https://www.googleapis.com/auth/drive';
 
-/**
- * Delay the execution of an api method after the api is ready.
- */
-GDrive.prototype.whenReadyCall = function(delayedFunction) {
-    this.delayedQueue.push(delayedFunction);
-};
-
-/**
- * Notify that the drive api is ready to be called. Processes any pending calls than may have been queued up.
- */
-GDrive.prototype.setReady = function() {
-    this.ready = true;
-    for (var i = 0; i < this.delayedQueue.length; i++) {
-        this.delayedQueue[i]();
-    }
-    this.delayedQueue = null;
-};
-
-/**
- * Check if the current user has authorized the application.
- */
-GDrive.prototype.auth = function(immediate, callback) {
-    gapi.auth.authorize({
-        'client_id': GDrive.CLIENT_ID,
-        'scope': GDrive.SCOPES,
-        'immediate': immediate
-    }, function(authResult) {
-        callback(authResult && !authResult.error);
-    });
-};
-
-GDrive.prototype.listTrustpadFiles = function(callback) {
-    if (!this.ready) {
-        this.whenReadyCall(this.listTrustpadFiles.bind(this, callback));
-        return;
-    }
-
-    var request = gapi.client.drive.files.list({
-        'q': 'title contains \'.trustpad\' and not trashed'
-    }).then(
-        function(res) {
-          console.log(res.result);
-          callback(res.result.items);
-        },
-        function(error) {
-          console.log('error: ' + error);
-        }
-    );
-};
-
-GDrive.prototype.writeFile = function(fileId, data, callback) {
-    const boundary = '-------314159265358979323846';
-    const delimiter = "\r\n--" + boundary + "\r\n";
-    const closeDelim = "\r\n--" + boundary + "--";
-    var metadata = {
-        'title': 'test.trustpad',
-        'mimeType': 'text/plain'
-    };
-    var body = delimiter +
-        'Content-Type: application/json\r\n\r\n' +
-        JSON.stringify(metadata) +
-        delimiter +
-        'Content-Type: text/plain\r\n' +
-        'Content-Transfer-Encoding: base64\r\n' +
-        '\r\n' +
-        btoa(data) +
-        closeDelim;
-    var url = '/upload/drive/v2/files';
-    if (fileId) {
-        url += '/' + fileId;
-    }
-    var request = gapi.client.request({
-        'path': url,
-        'method': fileId ? 'PUT' : 'POST',
-        'params': { 'uploadType': 'multipart' },
-        'headers': { 'Content-Type': 'multipart/mixed; boundary="' + boundary + '"' },
-        'body': body
-    }).then(
-        function(res) {
-            callback(res.id);
-        },
-        function(error) {
-            console.log('error: ' + error);
-        }
-    );
-};
-
-GDrive.prototype.readFile = function(fileId, callback) {
-    if (!this.ready) {
-        this.whenReadyCall(this.readFile.bind(this, fileId, callback));
-        return;
-    }
-
-    var request = gapi.client.drive.files.get({
-        'fileId': fileId,
-        'alt': 'media'
-    }).then(
-        function(res) {
-            callback(res.body);
-        },
-        function(error) {
-            console.log('error: ' + error);
-        }
-    );
-};
